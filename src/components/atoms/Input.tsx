823d9b1acb85a3bb6e818706f3948eba32c07bbd
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-09-18 23:04:29 +0400

    feat(login): add draft login form UI

---
+++ src/components/atoms/Input.tsx
@@ -0,0 +1,70 @@
+"use client";
+import { useRef, useEffect, useState } from "react";
+import Image from "next/image";
+
+export const Input = ({
+  placeholder = "",
+  required = false,
+  hasEyeIcon = false,
+  name,
+  type,
+}: {
+  placeholder?: string;
+  required?: boolean;
+  hasEyeIcon?: boolean;
+  name: string;
+  type: string;
+}) => {
+  const hiddenSpanRef = useRef<HTMLSpanElement>(null);
+  const [textWidth, setTextWidth] = useState(0);
+  const [userInput, setUserInput] = useState("");
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (hiddenSpanRef.current) {
+      setTextWidth(hiddenSpanRef.current.offsetWidth);
+    }
+  }, [placeholder]);
+  console.log("user input: ", userInput);
+
+  return (
+    <div className="relative">
+      <input
+        name={name}
+        value={userInput}
+        onChange={(e) => setUserInput(e.target.value)}
+        className="h-[42px] w-full rounded-xl border border-[#E1DFE1] focus:border-[#10151F] focus:outline-none placeholder-[#3E424A]"
+        type={
+          type == "password" ? (showPassword ? "text" : "password") : "text"
+        }
+        placeholder={placeholder}
+        style={{
+          textIndent: "8px",
+        }}
+      />
+      <span
+        ref={hiddenSpanRef}
+        className="absolute invisible text-[#3E424A]"
+        style={{ left: "12px", top: "50%", transform: "translateY(-50%)" }}
+      >
+        {placeholder}
+      </span>
+      <span
+        className={`absolute top-1/2 -translate-y-1/2 text-red-500 pointer-events-none ${userInput && "invisible"}`}
+        style={{
+          left: `${textWidth + 16}px`,
+        }}
+      >
+        *
+      </span>
+      {type == "password" && (
+        <button
+          className="absolute right-2 top-1/2 -translate-y-1/2 text-red-500 cursor-pointer"
+          onClick={() => setShowPassword((prev) => !prev)}
+        >
+          <Image alt="icon" width={17} height={12.5} src="/eye.svg" />
+        </button>
+      )}
+    </div>
+  );
+};
