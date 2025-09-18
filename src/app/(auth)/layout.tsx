790caaeb77f72283322118b5bb77a4f40b53cc0a
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-09-18 16:46:07 +0400

    feat(login): add cover image

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,16 @@
+import Image from "next/image";
+
 export default function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <>{children}</>;
+  return (
+    <div className="flex">
+      <div className="w-[948px] h-[1000px] relative">
+        <Image src="/loginImg.png" alt="login-image" priority fill />
+      </div>
+      {children}
+    </div>
+  );
 }
