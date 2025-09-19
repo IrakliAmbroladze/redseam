7005bf9d67751b096fe56072f8cee6afc9cb8e21
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-09-19 23:11:06 +0400

    feat(home): add headeirline to products page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Filters } from "@/components";
 export default function Home() {
-  return <></>;
+  return (
+    <div className="px-[100px] flex justify-between">
+      <h1 className="text-[64px] font-semibold">Products</h1>
+      <Filters />
+    </div>
+  );
 }
--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,2 +1,3 @@
 export { Input } from "./Input";
 export { FieldList } from "./FieldList";
+export { Filters } from "./Filters";
