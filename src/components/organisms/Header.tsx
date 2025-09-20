7e7cd59d11877b1eae9cf438cfd34f5ed8c01328
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-09-20 22:32:57 +0400

    feat(header): add client component cart icon

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { CartIcon } from "../atoms";
 export const Header = () => {
   return (
     <nav className="fixed h-20 w-[1920px] z-50 flex items-center justify-between py-2.5 px-[100px] bg-white">
@@ -9,12 +10,15 @@ export const Header = () => {
         </div>
         <span className="text-[16px] font-semibold ">RedSeam Clothing</span>
       </Link>
-      <Link href="/login" className="flex items-center text-[#10151F]">
-        <div className="w-[20px] h-[20px] relative">
-          <Image src="/user.svg" alt="logo" fill />
-        </div>
-        <span className="text-xs">Log in</span>
-      </Link>
+      <div className="flex gap-5">
+        <CartIcon />
+        <Link href="/login" className="flex items-center text-[#10151F]">
+          <div className="w-[20px] h-[20px] relative">
+            <Image src="/user.svg" alt="logo" fill />
+          </div>
+          <span className="text-xs">Log in</span>
+        </Link>
+      </div>
     </nav>
   );
 };
