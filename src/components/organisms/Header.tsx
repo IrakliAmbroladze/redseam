57b6ba60e7d9a15c4c579b644ba6ceeddb547095
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-09-20 17:51:05 +0400

    style(header): make background white not transparant

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 export const Header = () => {
   return (
-    <nav className="fixed h-20 w-[1920px] z-50 flex items-center justify-between py-2.5 px-[100px]">
+    <nav className="fixed h-20 w-[1920px] z-50 flex items-center justify-between py-2.5 px-[100px] bg-white">
       <Link href="/" className="flex items-center text-[#10151F]">
         <div className="w-[24px] h-[24px] relative">
           <Image src="/HandEye.svg" alt="logo" fill />
