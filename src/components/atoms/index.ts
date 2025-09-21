60454e0e90671de92c9a9af9a765fcc034731fed
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-09-21 23:10:34 +0400

    refac(headline): extract as component

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,3 +1,4 @@
 export { CartIcon } from "./CartIcon";
 export { SubmitButton } from "./SubmitButton";
 export { OrangeButton } from "./OrangeButton";
+export { Headline } from "./Headline";
--- src/components/molecules/ProductsHeadLine.tsx
+++ src/components/molecules/ProductsHeadLine.tsx
@@ -1,8 +1,9 @@
 import { Filters } from "@/components";
+import { Headline } from "../atoms";
 export const ProductsHeadLine = () => {
   return (
     <div className="flex justify-between">
-      <h1 className="text-[64px] font-semibold">Products</h1>
+      <Headline>Products</Headline>
       <Filters />
     </div>
   );
