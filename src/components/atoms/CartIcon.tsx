e6ef8030f786871def648b8f095b315b67c1ca92
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-09-21 04:18:56 +0400

    feat(header): add isShown useState hook for cart

--- src/components/atoms/CartIcon.tsx
+++ src/components/atoms/CartIcon.tsx
@@ -1,14 +1,16 @@
 "use client";
+import { useState } from "react";
 import { CartIconBlack } from "../icons";
+import { Cart } from "../organisms";
 
 export const CartIcon = () => {
+  const [isShown, setIsShown] = useState<boolean>(false);
   return (
-    <button
-      onClick={() => {
-        alert("hello");
-      }}
-    >
-      <CartIconBlack />
-    </button>
+    <>
+      <button onClick={() => setIsShown((prev) => !prev)}>
+        <CartIconBlack />
+      </button>
+      {isShown && <Cart />}
+    </>
   );
 };
