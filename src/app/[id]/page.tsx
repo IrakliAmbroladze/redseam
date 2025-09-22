53617f2943058759569df57fd60d97864108c7e7
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-09-22 11:16:05 +0400

    refac(product-page): extract getProduct function

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,5 +1,5 @@
-import { Product } from "@/types";
 import { ProductInfo } from "@/components";
+import { getProduct } from "@/lib";
 import Image from "next/image";
 
 export default async function Page({
@@ -8,23 +8,7 @@ export default async function Page({
   params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
-  const getProduct = async () => {
-    try {
-      const response = await fetch(
-        `https://api.redseam.redberryinternship.ge/api/products/${id}`,
-      );
-      const data: Product = await response.json();
-      return data;
-    } catch (error) {
-      if (error instanceof Error) {
-        console.warn("error while fetching a product: ", error.message);
-      } else {
-        console.log("Uknown error while fetching a product");
-      }
-    }
-  };
-  const data = await getProduct();
-  console.log("data is: ", data);
+  const data = await getProduct(id);
   if (!data)
     return (
       <div className="w-full min-h-dvh flex justify-center items-center ">
@@ -37,28 +21,20 @@ export default async function Page({
       <div className="flex justify-between">
         <div className="flex gap-2.5">
           <div className="flex flex-col gap-[9px]">
-            {data &&
-              data.images.map((image) => (
-                <Image
-                  key={image}
-                  src={image}
-                  width={121}
-                  height={161}
-                  alt="image"
-                />
-              ))}
+            {data.images.map((image) => (
+              <Image
+                key={image}
+                src={image}
+                width={121}
+                height={161}
+                alt="image"
+              />
+            ))}
           </div>
-          {data && (
-            <Image
-              src={data.cover_image}
-              width={703}
-              height={937}
-              alt="image"
-            />
-          )}
+          <Image src={data.cover_image} width={703} height={937} alt="image" />
         </div>
         <div className="w-[703px]">
-          {data && <ProductInfo product={data} />}
+          <ProductInfo product={data} />
         </div>
       </div>
     </div>
