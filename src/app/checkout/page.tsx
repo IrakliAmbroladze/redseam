e4e3e464d34d851d1098188c2979747dd6baaed8
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-09-22 13:07:32 +0400

    feat(checkout): import CartBoard

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,12 +1,20 @@
-import { Headline } from "@/components/atoms";
+import { CartBoard } from "@/components";
+import { Headline, OrangeButton } from "@/components/atoms";
 
 const CheckoutPage = () => {
   return (
     <div className="px-[100px]">
       <Headline> Checkout</Headline>
-      <div className="flex justify-between">
-        <div>Order details</div>
-        <div>cart</div>
+      <div className="flex justify-between h-[635px]">
+        <div className="h-[635px] w-[1129px] bg-[#F8F6F7] rounded-2xl">
+          Order details
+        </div>
+        <div className="h-[635px] w-[460px] flex flex-col gap-20">
+          <CartBoard />
+          <OrangeButton px={60} py={16}>
+            pay
+          </OrangeButton>
+        </div>
       </div>
     </div>
   );
