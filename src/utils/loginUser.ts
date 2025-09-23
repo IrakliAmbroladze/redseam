767cf21e05ade85af90b7b13aee1d54733874658
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-09-23 14:32:19 +0400

    feat(login): update loginUser function for real login

--- src/utils/loginUser.ts
+++ src/utils/loginUser.ts
@@ -1,8 +1,25 @@
 "use server";
+
+import { redirect } from "next/navigation";
+
 export const loginUser = async (prevState: unknown, formData: FormData) => {
   const email = formData.get("email");
   const password = formData.get("password");
   console.log("Email:", email, "Password:", password);
+  const response = await fetch(
+    "https://api.redseam.redberryinternship.ge/api/login",
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    },
+  );
 
+  if (response.ok) {
+    console.log("response JSON: ", await response.json());
+    redirect("/");
+  } else {
+    console.log("not logged in");
+  }
   return { message: "Login processed" };
 };
