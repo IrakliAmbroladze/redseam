58047a3b013c560b1c196d8dc551451eaf74d4d5
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-09-26 10:04:40 +0400

    feat(header): update user avatar in header

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,7 +1,16 @@
+"use server";
+
 import Image from "next/image";
 import Link from "next/link";
 import { CartIcon } from "../atoms";
-export const Header = () => {
+import { cookies } from "next/headers";
+
+export const Header = async () => {
+  const cookieStore = await cookies();
+  const token = cookieStore.get("token");
+  const avatar = cookieStore.get("avatar");
+  const isLoggedIn = Boolean(token?.value);
+  const hasAvatar = Boolean(avatar?.value);
   return (
     <nav className="fixed h-20 w-[1920px] z-50 flex items-center justify-between py-2.5 px-[100px] bg-white">
       <Link href="/" className="flex items-center text-[#10151F]">
@@ -12,11 +21,12 @@ export const Header = () => {
       </Link>
       <div className="flex gap-5">
         <CartIcon />
+        <div className="w-[20px] h-[20px] relative">
+          {hasAvatar ? "avatar" : <Image src="/user.svg" alt="logo" fill />}
+        </div>
+
         <Link href="/login" className="flex items-center text-[#10151F]">
-          <div className="w-[20px] h-[20px] relative">
-            <Image src="/user.svg" alt="logo" fill />
-          </div>
-          <span className="text-xs">Log in</span>
+          {!isLoggedIn && <span className="text-xs">Log in</span>}
         </Link>
       </div>
     </nav>
