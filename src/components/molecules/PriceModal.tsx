a82ad9e96cd3b0568ef7235efec48df8a9ab8074
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-09-27 23:04:54 +0400

    refac(filter): use useSerachParams and useRouter in price filters

--- src/components/molecules/PriceModal.tsx
+++ src/components/molecules/PriceModal.tsx
@@ -1,19 +1,42 @@
+import { useRouter, useSearchParams } from "next/navigation";
 import { OrangeButton } from "../atoms";
 import { Input } from "./Input";
 
-export const PriceModal = () => (
-  <div className="flex flex-col w-[392px] bg-white absolute top-10 -right-3.5 rounded-lg border border-[#E1DFE1] p-4 gap-5">
-    <h6 className="text-[14px] font-semibold text-start">Select price</h6>
-    <form className="flex flex-col gap-5 items-end">
-      {/* <input type="number" id="from" name="from" />
-      <input type="number" id="to" name="to" />*/}
-      <div className="flex gap-2.5">
-        <Input placeholder="from" required name="from" type="number" />
-        <Input placeholder="to" required name="to" type="number" />
-      </div>
-      <div className="w-32 ">
-        <OrangeButton type="submit"> apply</OrangeButton>
-      </div>
-    </form>
-  </div>
-);
+export const PriceModal = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const handleApply = (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const from = formData.get("from");
+    const to = formData.get("to");
+    const params = new URLSearchParams(searchParams);
+    if (from) {
+      params.set("from", from.toString());
+    } else {
+      params.delete("from");
+    }
+
+    if (to) {
+      params.set("to", to.toString());
+    } else {
+      params.delete("to");
+    }
+
+    router.push(`?${params.toString()}`);
+  };
+  return (
+    <div className="flex flex-col w-[392px] bg-white absolute top-10 -right-3.5 rounded-lg border border-[#E1DFE1] p-4 gap-5">
+      <h6 className="text-[14px] font-semibold text-start">Select price</h6>
+      <form onSubmit={handleApply} className="flex flex-col gap-5 items-end">
+        <div className="flex gap-2.5">
+          <Input placeholder="from" required name="from" type="number" />
+          <Input placeholder="to" required name="to" type="number" />
+        </div>
+        <div className="w-32 ">
+          <OrangeButton type="submit"> apply</OrangeButton>
+        </div>
+      </form>
+    </div>
+  );
+};
