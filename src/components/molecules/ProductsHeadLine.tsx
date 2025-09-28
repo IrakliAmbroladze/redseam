ed036a75a8f2739671502aeac096f554347edaeb
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-09-28 04:28:12 +0400

    feat(listing): show number of result dynamically

--- src/components/molecules/ProductsHeadLine.tsx
+++ src/components/molecules/ProductsHeadLine.tsx
@@ -1,10 +1,11 @@
 import { Filters } from "@/components";
 import { Headline } from "../atoms";
-export const ProductsHeadLine = () => {
+import { Meta } from "@/types";
+export const ProductsHeadLine = ({ metaData }: { metaData: Meta }) => {
   return (
     <div className="flex justify-between">
       <Headline>Products</Headline>
-      <Filters />
+      <Filters from={metaData.from} to={metaData.to} total={metaData.total} />
     </div>
   );
 };
